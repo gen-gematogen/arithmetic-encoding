b2bb4396c022a3ed1cf2a1d9fc4762e36fb413a1
Gennady <shutkov6914@gmail.com>
2023-12-16 12:02:41 +0300

    first try to write arithmetic encoding

---
+++ arithmetic_encoding.cpp
@@ -0,0 +1,50 @@
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+std::unordered_map<char, double> to_cum_prob(std::unordered_map<char, double> &prob){
+  std::unordered_map<char, double> cum_prob;
+  cum_prob[0] = 0;
+  for (int c = 1; c <= 127; ++c){
+    cum_prob[(char)c] = cum_prob[c - 1] + prob[c - 1];
+  }
+  return cum_prob;
+}
+
+std::vector<double> encode(std::string msg, std::unordered_map<char, double> prob){
+  double low = 0, high = 1;
+  auto cum_prob = to_cum_prob(prob);
+
+  std::cout << "---\n";
+  for (char c = 0; c < 127; ++c){
+    std::cout << c << "  " << prob[c] << " " << cum_prob[c] << "\n";
+  }
+  std::cout << "---\n";
+
+  for (char l: msg){
+    double new_low = high * cum_prob[l];
+    double new_high = high * (l < 127 ? cum_prob[l + 1] : 1);
+    low = new_low;
+    high = new_high;
+  }
+
+  return {low, high};
+}
+
+int main(){
+  std::unordered_map<char, double> test_prob;
+
+  test_prob[0] = 0.5;
+  test_prob[1] = 0.5;
+
+  std::string test_msg = std::to_string(char(1));
+
+  auto ans = encode(test_msg, test_prob);
+
+  std::cout << ans[0] << " " << ans[1] << "\n";
+
+  return 0;
+}
