59f9d4aa58d356e1af105fb3fc42fdcc89b9fdb4
Gennady <shutkov6914@gmail.com>
2023-12-19 21:27:48 +0300

    finished encoding. Has problems with roundof

--- arithmetic_encoding.cpp
+++ arithmetic_encoding.cpp
@@ -1,21 +1,76 @@
 #include <bitset>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <ostream>
 #include <sstream>
 #include <string>
 #include <unordered_map>
 #include <fstream>
+#include <vector>
 
 typedef unsigned long long ull;
 
 const static int N = 1000;
-const static char eof = 0;
+const static char eof = 'c';
 const static ull prec = 31;
 const static ull whole = 1 << (prec - 1);
 const static ull half = whole >> 1;
 const static ull quart = whole >> 2;
 const static ull R = 1<<30; 
+
+class data_buf{
+public: 
+  int w_bit = 0, r_bit = 0, r_len = 0;
+  std::vector<std::bitset<8>> buf;
+
+  data_buf(){
+    //buf.push_back(std::bitset<8> ());
+  }
+
+  void add(bool v){
+    if (!buf.size()){
+      buf.push_back(std::bitset<8> ());
+    }
+
+    buf[buf.size() - 1][w_bit] = v;
+
+    w_bit++;
+
+    if (w_bit == 8){
+      buf.push_back(std::bitset<8> ());
+      w_bit = 0;
+    }
+  }
+
+  std::vector<std::bitset<8>> get(){
+    return buf;
+  }
+
+  int size(){
+    return buf.size();
+  }
+
+  void append(std::bitset<8> b){
+    buf.push_back(b);
+  }
+
+  bool next(){
+    bool v = buf[r_len][r_bit];
+
+    r_bit++;
+
+    if (r_bit == 8){
+      r_bit = 0;
+      r_len++;
+    }
+
+    return v;
+  }
+
+  ~data_buf(){
+  }
+};
 
 std::unordered_map<char, ull> to_cum_prob(std::unordered_map<char, ull> &prob){
   std::unordered_map<char, ull> cum_prob;
@@ -36,12 +91,12 @@ ull round_div(ull a, ull b){
   return a / b;
 }
 
-std::pair<std::bitset<N>, int> encode(std::string msg, std::unordered_map<char, ull> prob){
+data_buf encode(std::string msg, std::unordered_map<char, ull> prob){
   int k = 0;
   ull min = 0, max = whole, s = 0, w;
-  std::bitset<N> bin_repr;
-  
-  bin_repr.reset();
+  data_buf buf;
+
+  // bin_repr.reset();
 
   auto cum_prob = to_cum_prob(prob);
 
@@ -55,9 +110,11 @@ std::pair<std::bitset<N>, int> encode(std::string msg, std::unordered_map<char, 
         min = min << 1;
         max = max << 1;
         
-        bin_repr[k++] = 0;
+       //bin_repr[k++] = 0;
+        buf.add(0);
         for (int i = 0; i < s; ++i){
-          bin_repr[k++] = 1;
+          // bin_repr[k++] = 1;
+          buf.add(1);
         }
         
         s = 0;
@@ -67,9 +124,11 @@ std::pair<std::bitset<N>, int> encode(std::string msg, std::unordered_map<char, 
         min = (min - half) << 1;
         max = (max - half) << 1;
         
-        bin_repr[k++] = 1;
+        // bin_repr[k++] = 1;
+        buf.add(1);
         for (int i = 0; i < s; ++i){
-          bin_repr[k++] = 0;
+          // bin_repr[k++] = 0;
+          buf.add(0);
         }
         
         s = 0;
@@ -86,30 +145,36 @@ std::pair<std::bitset<N>, int> encode(std::string msg, std::unordered_map<char, 
   s++;
 
   if (min <= quart){
-    bin_repr[k++] = 0;
+    // bin_repr[k++] = 0;
+    buf.add(0);
     for (int i = 0; i < s; ++i){
-      bin_repr[k++] = 1;
+      // bin_repr[k++] = 1;
+      buf.add(1);
     }
   }
   else{
-    bin_repr[k++] = 1;
+    // bin_repr[k++] = 1;
+    buf.add(1);
     for (int i = 0; i < s; ++i){
-      bin_repr[k++] = 0;
+      // bin_repr[k++] = 0;
+      buf.add(0);
     }
   }
 
-  return {bin_repr, k};
+  //return {bin_repr, k};
+  return buf;
 }
 
-std::string decode(std::unordered_map<char, ull> prob, std::bitset<N> data, int k){
+std::string decode(data_buf data, std::unordered_map<char, ull> prob){
   int i = 0;
   ull min = 0, max = whole, value = 0, w;
   std::string ans = "";
+  int N = data.size() << 3;
 
   auto cum_prob = to_cum_prob(prob);
 
-  for (; i < prec - 1 && i < N && i < k; ++i){
-    if (data[i]){
+  for (; i < prec - 1 && i < N; ++i){
+    if (data.next()){
       value += ((ull)1)<<(prec - i - 2);
     }
   }
@@ -145,7 +210,7 @@ std::string decode(std::unordered_map<char, ull> prob, std::bitset<N> data, int 
         value = (value - half) << 1;
       }
 
-      if (i < N && data[i]){
+      if (i < N && data.next()){
         value++;
       }
       i++;
@@ -156,7 +221,7 @@ std::string decode(std::unordered_map<char, ull> prob, std::bitset<N> data, int 
       max = (max - quart) << 1;
       value = (value - quart) << 1;
 
-      if (i < N && data[i]){
+      if (i < N && data.next()){
         value++;
       }
       i++;
@@ -167,62 +232,100 @@ std::string decode(std::unordered_map<char, ull> prob, std::bitset<N> data, int 
 }
 
 int main(int argc, char **argv){
+  std::unordered_map<char, ull> prob;
+
+  prob['a'] = prob['b'] = (R - (R >> 7)) >> 1;
+  prob[eof] = R >> 7;
+
   if (argc == 1){
     std::cerr << "Mode not specified\n";
     return 1;
   }
 
-  if (argv[1] == "-e" || argv[1] == "--encode"){
+  if (strcmp(argv[1], "-e") == 0 || strcmp(argv[1], "--encode") == 0){
     std::string input_file = argv[2];
     std::string output_file = argv[3];
 
+    std::cout << "Opening file...\n";
+  
     std::ifstream in_stream;
     in_stream.open(input_file, std::ios::in);
 
+    std::cout << "Reading data...\n"; 
+
     std::stringstream data;
     data << in_stream.rdbuf();
-    data << eof;
 
     in_stream.close();
 
     auto str_data = data.str();
+    str_data[str_data.size() - 1] = eof;
 
-    auto encoded_data = encode(std_data, prob);
+    std::cout << "Encoding...\n";
 
+    auto encoded_buf = encode(str_data, prob);
+    auto encoded_str = encoded_buf.get();
+
+    std::cout << "Writing data to binary file...\n";
+
+    std::ofstream out_stream;
+    out_stream.open(output_file, std::ios::out | std::ios::binary);
+
+    for (int i = 0; i < encoded_str.size(); ++i){
+      out_stream << (char)encoded_str[i].to_ullong();
+    }
+
+    out_stream.close();
+
+    std::cout << "Done!\n";
+
+    return 0;
   }
-  else if (argv[1] == "-d" || argv[1] == "--decode"){
+  else if (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--decode") == 0){
+    std::string input_file = argv[2];
+    std::string output_file = argv[3];
 
+    std::cout << "Opening file...\n";
+  
+    std::ifstream in_stream;
+    in_stream.open(input_file, std::ios::in | std::ios::binary);
+
+    std::cout << "Reading data...\n"; 
+
+    unsigned char c;
+    data_buf encoded_data;
+
+    in_stream >> c;
+    while (!in_stream.eof()){
+      std::bitset<8> cur((ull) c);
+      encoded_data.append(cur);
+      in_stream >> c;
+    }
+
+    in_stream.close();
+
+    std::cout << "Decoding...\n";
+
+    auto decoded_str = decode(encoded_data, prob);
+
+    std::cout << "Writing data to output file...\n";
+
+    std::ofstream out_stream;
+    out_stream.open(output_file, std::ios::out);
+    decoded_str.pop_back();
+
+    out_stream << decoded_str;
+
+    out_stream.close();
+
+    std::cout << "Done!\n";
+ 
+    return 0;
   }
   else{ 
     std::cerr << "No such mode. Please select -e/--encode or -d/--decode.\n";
     return 1;
   }
-
-  std::unordered_map<char, ull> test_prob;
-
-  // for (char c = 'a'; c <= 'z'; ++c){
-  //   test_prob[c] = 0.9 / ('z' - 'a' + 1);
-  // }
-  test_prob['a'] = test_prob['b'] = (R - (R >> 7)) >> 1;
-  test_prob[eof] = R >> 7;
-  // test_prob[' '] = 0.05;
-
-  std::string test_msg;
-
-  std::cin >> test_msg;
-  test_msg += eof;
-
-  auto ans = encode(test_msg, test_prob);
-
-  for (int i = 0; i < ans.second; ++i){
-    std::cout << ans.first[i];
-  }
-  std::cout << "\n";
-  std::fflush(stdout);
-
-  auto dec_s = decode(test_prob, ans.first, ans.second);
-
-  std::cout << dec_s << "\n";
 
   return 0;
 }
